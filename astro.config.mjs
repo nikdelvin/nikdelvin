f64ab7104867e3f4aa16c1ce67a180161913038a
nikdelvin <erter.live@gmail.com>
2024-11-21 11:44:45 +0400

    Domain change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nikdelvin.com',
+    site: 'https://nikdelv.in',
     integrations: [tailwind()]
 })
