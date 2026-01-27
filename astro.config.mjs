706f9c72006bfbccf09f7e66ff79ccc97d354249
nikdelvin <the@nikdelv.in>
2026-01-28 00:28:53 +0400

    Domain Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nikdelv.in',
+    site: 'https://portfolio.by.nikdelv.in',
     integrations: [tailwind()],
     devToolbar: {
         enabled: false
